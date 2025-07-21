4cfab7bb80a83740387a6c8230416a2d156a0028
Jonathan Maia <jonathanmaia@gmail.com>
2025-07-21 19:55:34 -0300

    Atualizando código

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,9 +1,42 @@
-import { Template } from '@/components';
+'use client';
+
+import { Template, ImageCard } from '@/components';
+import { useState } from 'react';
+import {Image } from "@/resources/image/image.resource";
+import { useImageService } from '@/resources/image/image.service';
 
 export default function GaleriaPage() {
+
+    const image1 = 'https://img.nsctotal.com.br/wp-content/uploads/2024/02/montanha-alpes-suicos.jpg';
+    const image2 = 'https://www.bsr.org/images/heroes/bsr-focus-nature-hero.jpg';
+
+    const [codigoImage, setCodigoImage] = useState<number>(1);
+    const [urlImage, setUrlImage] = useState<string>(image1);
+
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([]);
+
+    async function searchImages() {
+        setImages(await useService.buscar());
+    }
+
+    function renderImageCard(image: Image) {
+        return <ImageCard nome={image.name} tamanho={image.size} dataUpload={image.uploadDate} src={image.url} />
+    }
+
+    function renderImageCards() {
+        return images.map( renderImageCard );
+    }
+
     return (
         <Template>
-            <h1>Galeria</h1>
+            <button className = "bg-gray-500" onClick={searchImages}>Clique para mudar</button>
+
+            <section className="grid grid-cols-3 gap-8">
+                {
+                    renderImageCards()
+                }
+            </section>
         </Template>
     );
 }
